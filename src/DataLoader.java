31f0f806d0c6f398ac4966acee9005bae8f39e50
ayushr2 <ayushr2@uw.edu>
2026-01-14 13:19:54 -0800

    skeleton

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,10 +1,3 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
 /**
  * @author Chris Chun, Ayush
  * @version 1.1
